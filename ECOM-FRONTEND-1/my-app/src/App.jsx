c8821dc5bd6a018fd06f3b4caee275c74937d816
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-07-14 09:20:20 +0300

    fixed my react from ts to jsx having removed the prior files

---
+++ ECOM-FRONTEND-1/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-gray-200 text-2xl border-2'>
+        My Ecom website
+      </div>
+    </>
+  )
+}
+
+export default App
