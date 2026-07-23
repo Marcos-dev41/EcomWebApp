d566c6c30ae3b424029c36f490a7302236d985d5
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-07-24 00:37:57 +0300

    enaled async functionality to  be utilized in handling email service

--- ECOM-BACKEND-1/web/src/main/java/com/ecom/web/WebApplication.java
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/WebApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableAsync
 public class WebApplication {
 
 	public static void main(String[] args) {
