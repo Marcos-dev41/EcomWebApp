24517cb57149368f453f9c1aa17c761917008d6c
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-07-23 13:08:53 +0300

    created user and order relationship in the db

--- ECOM-BACKEND-1/web/src/main/java/com/ecom/web/model/Order.java
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/model/Order.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -32,5 +34,10 @@ public class Order {
 
  @OneToMany(mappedBy = "orders" ,cascade = CascadeType.ALL)
    private List <OrderItem> orderItems;
+
+@ManyToOne()
+@JoinColumn(name = "user_id")
+ private User user;
+
 
 }
--- ECOM-BACKEND-1/web/src/main/java/com/ecom/web/model/User.java
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/model/User.java
@@ -1,11 +1,15 @@
 package com.ecom.web.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,4 +31,7 @@ public class User {
     private String email;
     private String userPassword; 
     private String role = "USER";
+
+    @OneToMany(mappedBy = "user" , cascade = CascadeType.ALL)
+    private List<Order> order;
 }
