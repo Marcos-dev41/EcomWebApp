f238246b9286a4945d4b93a53e7bbd82bb180878
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-07-23 12:12:54 +0300

    extracting the callback result and updating the order status.....added a find by checkrequestby id method for querying the order db

--- ECOM-BACKEND-1/web/src/main/java/com/ecom/web/payment/PaymentController.java
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/payment/PaymentController.java
@@ -1,5 +1,6 @@
 package com.ecom.web.payment;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +40,46 @@ public class PaymentController {
     @PostMapping("/callback")
 public Map<String, Object> handleCallback(@RequestBody Map<String, Object> callbackData) {
     System.out.println("Daraja callback received: " + callbackData);
-    // next: extract result, update the matching Order's status
+    
+    Map<String,Object> body = (Map<String,Object>)callbackData.get("Body");
+    Map<String,Object> stkCallback = (Map<String,Object>)body.get("stkCallback");
+
+    String checkoutRequestId = (String) stkCallback.get("CheckoutRequestID");
+    int resultCode = (int) stkCallback.get("ResultCode");
+
+    Order order = orderRepo.findByCheckoutRequestId(checkoutRequestId).orElse(null);
+
+
+    if(order == null){
+        System.out.println("no matching order for checkoutRequestId: " +  checkoutRequestId);
+        return Map.of("ResultCode",0,"ResultDesc","Accepted");
+    }
+
+    if(resultCode == 0){
+    List<Map<String, Object>> items = (List<Map<String, Object>>)
+    ((Map<String, Object>) stkCallback.get("CallbackMetadata")).get("Item");
+        String mpesaReceiptNumber= null;
+        Long phoneNumber = null;
+
+        for(Map<String,Object>item :items){
+            String name = (String) item.get("Name");
+            if (name.equals("MpesaReceiptNumber")){
+                mpesaReceiptNumber = (String)item.get("Value");
+            }else if(name.equals("PhoneNumber"))
+            {
+                phoneNumber = ((Number) item.get("Value")).longValue();
+            }}
+            //handle email submission
+            order.setOrderStatus("PAID");
+            orderRepo.save(order);
+
+            
+    }else{
+        // mark order as failed and push notification
+        order.setOrderStatus("FAILED");
+        orderRepo.save(order);
+    }
+
     return Map.of("ResultCode", 0, "ResultDesc", "Accepted");
-}
-}
+
+}}
--- ECOM-BACKEND-1/web/src/main/java/com/ecom/web/repository/OrderRepo.java
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/repository/OrderRepo.java
@@ -1,9 +1,11 @@
 package com.ecom.web.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ecom.web.model.Order;
 
 public interface OrderRepo  extends JpaRepository <Order,Integer>{
-    
+     Optional<Order>findByCheckoutRequestId(String checkoutRequestId);
 }
