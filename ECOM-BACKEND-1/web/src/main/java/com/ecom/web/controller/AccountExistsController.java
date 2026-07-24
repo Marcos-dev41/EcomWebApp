e167ecf97f34e32f836a1b2622531e9ad6ed936a
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-07-24 23:39:02 +0300

    creatd classes for handling password reset requests

---
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/controller/AccountExistsController.java
@@ -0,0 +1,23 @@
+package com.ecom.web.controller;
+
+import com.ecom.web.model.*;
+import com.ecom.web.service.AccountExistsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AccountExistsController {
+    // @Autowired
+    // private AccountExistsService accountExistsService;
+    
+    // @RequestMapping("/account")
+    // public ResponseEntity<Boolean> accountExists(@RequestBody User user){
+    //     boolean exists = accountExistsService.accountExistsChecker(user.getEmail());
+    //     return ResponseEntity.ok(exists);
+    // }
+}
---
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/service/AccountExistsService.java
@@ -0,0 +1,14 @@
+package com.ecom.web.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.ecom.web.model.User;
+import com.ecom.web.repository.*;
+
+public class AccountExistsService {
+    
+// public boolean accountExistsChecker(User user){
+    // LoginRepo.findByEmail(user.getEmail());
+    // return true;
+// }
+}
