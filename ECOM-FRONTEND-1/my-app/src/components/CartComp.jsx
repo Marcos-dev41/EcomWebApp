0e57ad22d176f8561e1ff13f5c47d611d3b92224
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-07-27 17:27:36 +0300

    ampped up the ui  a bit

--- ECOM-FRONTEND-1/my-app/src/components/CartComp.jsx
+++ ECOM-FRONTEND-1/my-app/src/components/CartComp.jsx
@@ -14,14 +14,14 @@ const {removeFromCart} = useCart()
 });
 
   return (
-    <div className='flex flex-row p-5 rounded-xl justify-between w-full 2xl:max-w-[550px] h-30 border-2 m-5'>
-        <img src={cartimg} alt=""/>
+    <div className='flex flex-row p-5 rounded-xl justify-between w-full items-center 2xl:max-w-[550px] h-30 border-2 m-5  shadow-sm'>
+        <img src={cartimg} alt="" width={70} />
         <div className='flex flex-row justify-around w-90'>
-        <div className='p-1 flex-wrap w-50 items-center'>
-            <h2 className=' font-semibold text-xl text-center'>{item.prodName}</h2>
+        <div className='p-1  flex flex-col w-50 items-center'>
+            <h2 className=' font-bold text-sm text-center'>{item.prodName}</h2>
         </div>
 
-        <div className="flex  flex-col flex-start font-semibold justify-around">
+        <div className="flex  flex-col flex-start font-bold text-sm text-gray-900 justify-around ">
             <h4>{formatKSh.format(item.quantity * item.price)}</h4>
             <p>Qty: {item.quantity}</p>
             <h4 className = "text-green-500 ">InStock</h4>
