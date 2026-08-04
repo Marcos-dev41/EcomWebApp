80aef1f6fbdada9901a524ae68146a49e94f1074
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-08-04 14:38:06 +0300

    removed unnecessary imports

--- ECOM-BACKEND-1/web/src/main/java/com/ecom/web/controller/CheckoutController.java
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/controller/CheckoutController.java
@@ -4,8 +4,6 @@ import java.util.UUID;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
