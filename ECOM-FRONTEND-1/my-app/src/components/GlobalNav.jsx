e78cd245d6441a2936266adaf10ce0233bbe030f
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-08-10 10:12:15 +0300

    fixed login and logout state with correct reedirections , changed the cart icon and aded a item counter to show how many items are in the cart at any given time

--- ECOM-FRONTEND-1/my-app/src/components/GlobalNav.jsx
+++ ECOM-FRONTEND-1/my-app/src/components/GlobalNav.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import user from '../../src/assets/account.png'
-import cart from '../../src/assets/add-cart.png'
+import addedtocart from '../../src/assets/grocery-store.png'
+import cartimg from '../../src/assets/sell.png'
 import bell from '../../src/assets/notification.png'
 import store from '../../src/assets/store.png'
 import { Link } from 'react-router-dom'
+import { useCart } from '../pages/CartContext'
+
 
 export default function 
 () {
+   const {cart} = useCart();
     const jwtToken = localStorage.getItem("token")
+        const expiryDate = (() => {
+  if (!jwtToken) return null;
+
+  try {
+   
+    let payloadBase64 = jwtToken.split('.')[1];
+    if (!payloadBase64) return null;
+
+    const payload = JSON.parse(atob(payloadBase64));
+
+    return payload.exp ? new Date(payload.exp * 1000).toLocaleString() : null;
+  } catch (error) {
+    console.error("Invalid JWT token:", error);
+    return null;
+  }
+    })
+
+    
+
+  const currentTime = new Date().toLocaleString();
+
+
+  // function to handle user logging out
+
+    function logout(){
+        localStorage.removeItem("token");
+    }
+
+   
 
 
   return (
@@ -17,13 +50,18 @@ export default function
         <div className='flex flex-row justify-between pl-5 pr-5 gap-4'>
             <Link to={"/products"}><img src={store} alt="" width=" 33px" height="30px" className='opacity-85 hover:scale-102' /></Link>
             <Link to={"/notifications"} ><img src={bell} alt="" width=" 33px" height="30px" className='opacity-85 hover:scale-102' /></Link>
-            <Link to={"/cart"}><img src={cart} alt="" width=" 35px" height="30px" className='opacity-85 hover:scale-102 active:bg-red-400'/></Link>
-        </div>
+            <Link to={"/cart"}>
+                {cart.length > 0 ?
+                <div className='flex'>
+                  <img src={addedtocart} alt="" width=" 30px" height="30px" className='opacity-85 hover:scale-102'/> 
+                  <div className=' m-0 rounded-full text-sm bg-blue-400 h-5 w-5 p-0.5 text-center'>{cart.length}</div>
+                </div>:
+                <img src={cartimg} alt="" width=" 35px" height="35px" className='opacity-85 hover:scale-102'/>}
+            </Link>        </div>
         <div className='flex flex-row justify-between w-32 items-center'>
             <a href="#"><img src={user} alt="" width=" 35px" height="35px" className='opacity-90 hover:scale-102' /></a>
              <div className='flex flex-col items-center text-start h-12'>
-                    { jwtToken ? <h3 className='text-sm text-green-700 m-3 hover:scale-102 font-semibold'>logged in</h3> : <Link to={"/login"}><h3 className='font-semibold hover:scale-102 rounded bg-blue-600 p-2 text-gray-200 pl-3 pr-3 text-sm m-1'>Log in</h3></Link>}
-             </div>
+                {jwtToken && expiryDate() >= currentTime ? <Link to={"/login"}> <button className='text-base m-1 hover:scale-102 bg-red-600 p-2 font-semibold rounded' onClick={logout}>logout</button> </Link>: <Link to={"/login"}><h3 className='font-semibold hover:scale-102 rounded bg-blue-600 p-2 text-gray-200 pl-3 pr-3 text-sm m-1 '>Log in</h3></Link>}             </div>
         </div>
         
     </div>
--- ECOM-FRONTEND-1/my-app/src/components/NavBar.jsx
+++ ECOM-FRONTEND-1/my-app/src/components/NavBar.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import user from '../../src/assets/account.png'
-import addedcart from '../../src/assets/add-cart.png'
+import addedcart from '../../src/assets/grocery-store.png'
 import cartimg from '../../src/assets/sell.png'
 import bell from '../../src/assets/notification.png'
 import { Link } from 'react-router-dom'
 import { useCart } from '../pages/CartContext'
+import ProductList from '../pages/ProductPage'
 
-export default function 
-() {
+export default function NavBar({search , setSearch}) {
     const {cart} = useCart();
     const jwtToken = localStorage.getItem("token")
 
@@ -27,37 +27,44 @@ export default function
     console.error("Invalid JWT token:", error);
     return null;
   }
-    })()
+    })
 
     
 
   const currentTime = new Date().toLocaleString();
-  if(expiryDate >= currentTime){
-    localStorage.removeItem("token")
-  }
 
 
-// function to handle user logging out
+  // function to handle user logging out
 
     function logout(){
         localStorage.removeItem("token");
     }
 
+   
+
   return (
     <div className=' flex flex-row justify-around w-full bg-orange-500 h-fit p-1 items-center font-semibold shadow-md'>
         <h1 className='text-2xl'>MoniMart</h1>
-        <input className='bg-orange-100 rounded-2xl w-80 p-2 pl-5' type="search" placeholder='search...'/>
+
+        <input className='bg-orange-100 rounded-2xl w-80 p-2 pl-5' type="search" value={search} placeholder='search...' onChange={((e)=>{setSearch(e.target.value)})}/>
+
+
         <div className='flex flex-row justify-between pl-5 pr-5 gap-4'>
             <Link to={"/notifications"} ><img src={bell} alt="" width=" 35px" height="30px" className='opacity-85 hover:scale-102' /></Link>
             <Link to={"/cart"}>
-                {cart.length > 0 ? <img src={addedcart} alt="" width=" 35px" height="35px" className='opacity-85 '/>:<img src={cartimg} alt="" width=" 35px" height="35px" className='opacity-85 hover:scale-102'/>}
+                {cart.length > 0 ?
+                <div className='flex'>
+                  <img src={addedcart} alt="" width=" 30px" height="30px" className='opacity-85 hover:scale-102'/> 
+                  <div className=' m-0  rounded-full text-sm bg-blue-400 h-5 w-5 p-0.5 text-center'>{cart.length}</div>
+                </div>:
+                <img src={cartimg} alt="" width=" 35px" height="35px" className='opacity-85 hover:scale-102'/>}
             </Link>
         </div>
         <div className='flex flex-row justify-between w-fit items-center'>
             <img src={user} alt="" width=" 35px" height="35px" className='opacity-90 hover:scale-102 mr-1' />
             <div className='flex flex-col items-center text-start h-12'>
                 
-                {jwtToken && expiryDate >= currentTime ? <Link to={"/login"}> <button className='text-base m-1 hover:scale-102 bg-red-600 p-2 font-semibold rounded' onClick={logout}>logout</button> </Link>: <Link to={"/login"}><h3 className='font-semibold hover:scale-102 rounded bg-blue-600 p-2 text-gray-200 pl-3 pr-3 text-sm m-1 '>Log in</h3></Link>}
+                {jwtToken && expiryDate() >= currentTime ? <Link to={"/login"}> <button className='text-base m-1 hover:scale-102 bg-red-600 p-2 font-semibold rounded' onClick={logout}>logout</button> </Link>: <Link to={"/login"}><h3 className='font-semibold hover:scale-102 rounded bg-blue-600 p-2 text-gray-200 pl-3 pr-3 text-sm m-1 '>Log in</h3></Link>}
                 
             </div>
         </div>
