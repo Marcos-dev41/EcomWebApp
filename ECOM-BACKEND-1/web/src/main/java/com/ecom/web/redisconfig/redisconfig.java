d071306ea639bed45f5fb79729c2010f69208e6b
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-08-20 12:39:11 +0300

    added in redis config and fixed some varibales onthe application properties pertaining to redis

---
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/redisconfig/redisconfig.java
@@ -0,0 +1,14 @@
+package com.ecom.web.redisconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+public class redisconfig {
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory){
+        return RedisCacheManager.builder(connectionFactory).build();
+    }
+}
