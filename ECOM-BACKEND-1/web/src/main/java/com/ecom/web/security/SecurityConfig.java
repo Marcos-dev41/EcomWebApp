1d89aaf013d7f1eb7dcffbb2599389c7e8c32e6a
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-09-02 21:54:28 +0300

    cors

--- ECOM-BACKEND-1/web/src/main/java/com/ecom/web/security/SecurityConfig.java
+++ ECOM-BACKEND-1/web/src/main/java/com/ecom/web/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
 public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-    configuration.setAllowedOrigins(List.of("https://26067a01.ecomwebapp.pages.dev"));
+    configuration.setAllowedOrigins(List.of("https://fd9abc0e.ecomwebapp.pages.dev"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
